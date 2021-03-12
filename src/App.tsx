ea5a517e9904f66f80ba13c381ad233059592a40
Francis <yiyboy@qq.com>
2021-03-12 17:47:12 +0800

    feat: 完成一些基础配置。

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styles from './App.module.css'
+import robots from './mockdata/robots.json'
+import Robot from './components/Robot'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <ul>
+        {robots.map((r) => (
+          <Robot id={r.id} name={r.name} />
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
