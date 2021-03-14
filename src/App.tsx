1ded5d17b3707500180ff1776d07b920965e0def
Francis <yiyboy@qq.com>
2021-03-14 18:21:44 +0800

    feat: React 组件的一个生命周期

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
-import robots from './mockdata/robots.json'
+// import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 
@@ -12,6 +12,7 @@ interface State {
 }
 
 class App extends React.Component<Props, State> {
+  // 生命周期第一阶段：初始化
   constructor(props: Props) {
     super(props)
 
@@ -21,11 +22,28 @@ class App extends React.Component<Props, State> {
     }
   }
 
+  // 在组件创建好DOM元素以后，挂载进入页面时候使用
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((data) => this.setState({ robotGallery: data }))
   }
+
+  // 生命周期第二阶段：更新
+
+  // 在组件接收到一个新的prop（更新后）调用
+  // componentWillReceiveProps
+  // state getDerivedStateFromProps(nextProps, prevState){}
+  // shouldComponentUpdate(nextProps,nextState){
+  //   return nextState.count !== this.state.count
+  // }
+  // 组件更新后使用
+  componentDidUpdate(){}
+
+  // 生命周期第三个阶段：销毁
+  // 组件销毁后调用
+  // 可以当作构造函数 destructor来使用
+  componentWillUnmount(){}
 
   render() {
     return (
@@ -36,15 +54,23 @@ class App extends React.Component<Props, State> {
         </div>
         <button
           onClick={() => {
-            this.setState((preState,preProps)=>{return { count: preState.count + 1 }},()=>{
-              console.log("count",this.state.count);
-            })
-            this.setState((preState,preProps)=>{return { count: preState.count + 1 }},()=>{
-              console.log("count",this.state.count);
-            })
-           
+            this.setState(
+              (preState, preProps) => {
+                return { count: preState.count + 1 }
+              },
+              () => {
+                console.log('count', this.state.count)
+              }
+            )
+            this.setState(
+              (preState, preProps) => {
+                return { count: preState.count + 1 }
+              },
+              () => {
+                console.log('count', this.state.count)
+              }
+            )
           }}
-          
         >
           Click!
         </button>
