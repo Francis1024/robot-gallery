c21e12870cc19e5c9f5a391910fe69debb655b02
Francis <yiyboy@qq.com>
2021-03-14 17:00:33 +0800

    feat: 完成媒体资源的引入。

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import styles from './App.module.css'
+import logo from "./assets/images/logo.svg";
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
 
 function App() {
   return (
     <div className={styles.app}>
-      <ul>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo} />
+        <h1>罗伯特机器人标题</h1>
+      </div>
+      <div className={styles.robotList}>
         {robots.map((r) => (
-          <Robot id={r.id} name={r.name} />
+          <Robot id={r.id} name={r.name} key={r.id} />
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
