467195f4d391ffaa311a52f298a42d9d1c6f49d8
Francis <yiyboy@qq.com>
2021-03-14 19:18:31 +0800

    feat: 使用Effect 钩子异步请求数据。

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
 // import robots from './mockdata/robots.json'
-// import Robot from './components/Robot'
+import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0)
+  const [robotGallery, setRobotGallery] = useState<any>([])
+
+  useEffect(() => {
+    document.title = `点击了${count}次`
+  }, [count])
+
+  useEffect(() => {
+     fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    )
+    .then(response => response.json())
+    .then(data => setRobotGallery(data))
+   
+  }, [])
+  
 
   return (
     <div className={styles.app}>
@@ -23,11 +38,11 @@ const App: React.FC = () => {
       </button>
       <span>count:{count}</span>
       <ShoppingCart></ShoppingCart>
-      {/* <div className={styles.robotList}>
-          {this.state.robotGallery.map((r) => (
+      <div className={styles.robotList}>
+          {robotGallery.map((r) => (
             <Robot id={r.id} name={r.name} key={r.id} />
           ))}
-        </div> */}
+        </div>
     </div>
   )
 }
