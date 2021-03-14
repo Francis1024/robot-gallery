e3cd7dc383530a142491f9167260d47fc69ec175
Francis <yiyboy@qq.com>
2021-03-14 17:09:49 +0800

    feat: 创建class类组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styles from './App.module.css'
-import logo from "./assets/images/logo.svg";
+import logo from './assets/images/logo.svg'
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
+import ShoppingCart from './components/ShoppingCart'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} alt="logo" className={styles.appLogo} />
         <h1>罗伯特机器人标题</h1>
       </div>
+      <ShoppingCart></ShoppingCart>
       <div className={styles.robotList}>
         {robots.map((r) => (
           <Robot id={r.id} name={r.name} key={r.id} />
